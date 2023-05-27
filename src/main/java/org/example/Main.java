f6b66cbc2e97a2b4d1a05a3cf9db7d5d0e63a60c
MuhIbrahimm <baim21121994@gmail.com>
2023-05-27 21:36:02 +0800

    Memperbaiki kode dan menambahkan depedensi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -8,7 +10,7 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 4040), 0);
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8083), 0);
         httpServer.createContext("/test", new testHandler());
         httpServer.setExecutor(Executors.newSingleThreadExecutor());
         httpServer.start();
